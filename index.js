7a18ff2b26789af618c3c62461e4df095bb3a1b5
hamink <hamin@atomrigs.io>
2022-04-27 12:23:33 -0600

    adding tokens

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const { User } = require("./models/Users")
 
 const config = require('./config/key')
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected'))
@@ -28,6 +30,30 @@ app.post('/register', (req, res) => {
             })
         })
 
+})
+
+app.post('/login', (req, res) => {
+  console.log('test', req.body)
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message: "There is no user registered"
+      })
+    } 
+    user.comparePassword(req.body.password,(err, isMatch) => {
+      if(!isMatch){
+        console.log('test7')
+        return res.json({loginSuccess: false, message: "Password is wrong"})
+      }
+
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token).status(200).json({loginSuccess: true, userId: user._id})
+      })
+
+    })
+  })
 })
 
 app.listen(port, () => {
