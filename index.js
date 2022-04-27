9c958a8e76769a86680e5931cda169bb07bd590b
hamink <hamin@atomrigs.io>
2022-04-27 13:03:32 -0600

    finished

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/api/users/login', (req, res) => {
   })
 })
 
-app.get('/api/users/auth', auth , (res, req) => {
+app.get('/api/users/auth', auth , (req, res) => {
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? true: false,
@@ -68,6 +68,15 @@ app.get('/api/users/auth', auth , (res, req) => {
     role: req.user.role,
     image: req.user.image,
   })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({ _id: req.user._id }, {token: ""}, (err, user) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).send({ success: true })
+  })
+
 })
 
 app.listen(port, () => {
